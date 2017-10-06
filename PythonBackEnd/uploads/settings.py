12e97b65f94cd65d769e9928dbb8c72a7d5e5ce1
doyougnu <jmy6342@gmail.com>
2017-10-05 19:20:15 -0700

    adding postgres again

--- PythonBackEnd/uploads/settings.py
+++ PythonBackEnd/uploads/settings.py
@@ -82,8 +82,12 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tldrDb',
+        'USER': 'postgres',
+        'PASSWORD': 'iamthepostgresuserpassword', # also bad practice most likely
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
