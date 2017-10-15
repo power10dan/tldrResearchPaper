4f1b8227a64e0b9b2a31336bf59c7b84f763841e
doyougnu <jmy6342@gmail.com>
2017-10-15 12:19:16 -0700

    Bug fix to return bad requests on invalid input

--- uploads/views/views.py
+++ uploads/views/views.py
@@ -68,7 +68,10 @@ class CreateUser(APIView):
     def post(self, request, format="json"):
         serializer = UserSerializer(data=request.data)
 
+        # if serializer succeeds then save and change the response to success
         if serializer.is_valid():
             user = serializer.save()
             if user:
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
