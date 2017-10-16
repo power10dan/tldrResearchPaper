6c3abd7c2be445cbceeed5a896d464d5ed99cc0c
doyougnu <jmy6342@gmail.com>
2017-10-16 11:46:42 -0700

    adding auth libs and test default formats

--- uploads/settings/settings_dev.py
+++ uploads/settings/settings_dev.py
@@ -114,7 +114,10 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-    )
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
+    ),
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
 
 
