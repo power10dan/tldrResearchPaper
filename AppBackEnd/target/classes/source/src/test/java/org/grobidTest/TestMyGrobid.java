eafab644750ba701c02e504c843fd8f4479e069f
doyougnu <jmy6342@gmail.com>
2017-10-17 22:48:43 -0700

    adding target files

---
+++ AppBackEnd/target/classes/source/src/test/java/org/grobidTest/TestMyGrobid.java
@@ -0,0 +1,54 @@
+package org.grobidExample;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import py4j.GatewayServer;
+
+import org.junit.Test;
+import java.io.*;
+
+/**
+ *  @author Daniel Lin
+ */
+public class TestMyGrobid {
+
+	private String testPath = null;
+
+	@Test
+	public void testPDFXML() throws Exception{
+		// String pdfGroup = "/home/doyougnu/Downloads/";
+		// String pdfOutput = "/home/doyougnu/tmp/OutputXMLs";
+		// boolean conHead = false;
+		// boolean conCite = false;
+		// PDFXML pdfXML = new PDFXML();
+		// int finish = pdfXML.PDFXMLConverter(pdfGroup, pdfOutput, conHead, conCite);
+		// assert(finish != 1);
+		// pdfXML.close();
+
+	}
+
+
+	/*@Test
+	public void testHeaderBibTeX() throws Exception {
+		String pdfPath = "./src/test/resources/Wang_paperAVE2008.pdf";
+		ExampleBibTex example = new ExampleBibTex();
+		String result = example.runGrobid(new File(pdfPath), "header", false);
+		assertNotNull(result);
+		
+		System.out.println(result);
+		example.close();
+	}
+	
+	@Test
+	public void testCitationBibTeX() throws Exception {
+		String pdfPath = "./src/test/resources/Wang_paperAVE2008.pdf";
+		ExampleBibTex example = new ExampleBibTex();
+		String result = example.runGrobid(new File(pdfPath), "citation", false);
+		assertNotNull(result);
+		
+		System.out.println(result);
+		example.close();
+	}	*/
+}
