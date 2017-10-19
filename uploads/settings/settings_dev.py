c433e95dd3d921a79860819995ef8dd92a23209a
kresges <kresges@oregonstate.edu>
2017-10-18 17:19:41 -0700

    user authentication is working.

--- uploads/settings/settings_dev.py
+++ uploads/settings/settings_dev.py
@@ -41,8 +41,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
-    'corsheaders'
+    'corsheaders',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
 ]
+
+SITE_ID=1
 
 MIDDLEWARE_CLASSES = [
     'corsheaders.middleware.CorsMiddleware',
