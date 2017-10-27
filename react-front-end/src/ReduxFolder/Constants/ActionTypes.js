b4cf82ebc4b6455ab0da1c560fa7fdcdc6bf3063
doyougnu <jmy6342@gmail.com>
2017-10-27 10:44:47 -0700

    Separating out types and actions

---
+++ react-front-end/src/ReduxFolder/Constants/ActionTypes.js
@@ -0,0 +1,14 @@
+/*
+ *	Action types, currently at
+ *  create account, forgot password,
+ *  and login and authenticate
+ *
+ *
+ */
+
+export const LOGIN      = 'LOGIN';
+export const FORGOTPass = 'FORGOTPass';
+export const FORGOTACC  = 'FORGOTAcc';
+export const CREATEACC  = 'CREATEACC';
+export const UPLOADFILE = 'UPLOADFILE';
+export const DELETEFILE = 'DELETEFILE';
