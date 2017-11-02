638cb2c5776b44da2b4647b63a1f420b71aead75
doyougnu <jmy6342@gmail.com>
2017-11-02 10:40:00 -0700

    exposing authentication reducer

--- react-front-end/src/ReduxFolder/Reducers/root.js
+++ react-front-end/src/ReduxFolder/Reducers/root.js
@@ -1,10 +1,12 @@
 import loginReducer from "./LoginReducer.js";
 import isLoadingReducer from "./isLoadingReducer.js";
+import authentication from "./authentication"
 import { combineReducers } from 'redux';
 
 const tldrApp = combineReducers({
 	  loginReducer,
-    isLoadingReducer
+    isLoadingReducer,
+    authentication
 });
 
 export default tldrApp;
