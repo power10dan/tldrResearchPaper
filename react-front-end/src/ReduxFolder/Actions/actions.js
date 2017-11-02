885ab03138dd1ca0b46222e297c0b7e062e4d1a0
doyougnu <jmy6342@gmail.com>
2017-11-01 21:16:17 -0700

    Adding login action and helper function

--- react-front-end/src/ReduxFolder/Actions/actions.js
+++ react-front-end/src/ReduxFolder/Actions/actions.js
@@ -7,25 +7,82 @@ import * as types from '../Constants/ActionTypes';
 
 
  // Async Operations, Converting them to Redux Thunk
-export function LogInOp(url){
- 	return (dispatch) =>{
- 		  dispatch(isLoading(true));
- 		  fetch(url).then((response)=>{
- 			    if(!response.ok){
- 				      throw Error(response.statusText);
- 				      dispatch(isLoading(false));
- 			    }
- 			  dispatch(isLoading(false));
- 			    return response;
- 		  }).then((response)=> {
- 				  response.json();
- 		  }).then(()=> {
- 		  	  dispatch(LogInSuccess(true));
- 		  }).catch((err)=> {
- 		  	  dispatch(LogInFailed(false, err));
- 		  });
- 	};
- }
+// export function LogInOp(url){
+//  	return (dispatch) =>{
+//  		  dispatch(isLoading(true));
+//  		  fetch(url).then((response)=>{
+//  			    if(!response.ok){
+//  				      throw Error(response.statusText);
+//  				      dispatch(isLoading(false));
+//  			    }
+//  			  dispatch(isLoading(false));
+//  			    return response;
+//  		  }).then((response)=> {
+//  				  response.json();
+//  		  }).then(()=> {
+//  		  	  dispatch(LogInSuccess(true));
+//  		  }).catch((err)=> {
+//  		  	  dispatch(LogInFailed(false, err));
+//  		  });
+//  	};
+// }
+
+/* helper login function takes a username and password, sends a request
+   asynchronously validates the response, and stores the auth token locally if
+   success, then return the user
+*/
+function _login(username, password) {
+    // set the request options
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+    };
+
+    // set the url and use fetch to send request
+		let url = "http://127.0.0.1:8000/rest-auth/login/";
+    return fetch(url, requestOptions)
+        .then(response => {
+
+            // if response is bad then return the status text
+            if (!response.ok) {
+                return Promise.reject(response.statusText);
+            }
+
+            // if good then return the response json
+            return response.json;
+        })
+
+    // catch the json which is a user
+        .then(user => {
+            // if user is good and we have a token, save token which is the same
+            // as being logged in
+            if (user && user.token) {
+                localStorage.setItem('user', JSON.stringify(user));
+            }
+
+            return user;
+        });
+}
+
+
+/* actual login function
+*/
+export function login(username, password) {
+    return dispatch => {
+        dispatch(request({ username }));
+
+        _login(username, password)
+            .then(
+                user => {
+                    dispatch(LogInSuccess(true));
+                },
+                error => {
+                    dispatch(LogInFailed(false, err));
+                });
+        
+    };
+}
 
  // ================ Async Operations, Converting them to Redux Thunk ===========
  // action dispatch for when login succeds
@@ -52,23 +109,6 @@ export function isLoading(isLoadingStats){
 		isLoading: isLoadingStats
 	};
 }
-
-//  make LogIn action return a function; THUNK!
-// export function LogInOp(url){
-//  	return (dispatch) =>{
-//  		  dispatch(isLoading(true));
-//  		fetch(url).then((response)=>{
-//  			if(!response.ok){
-//  				throw Error(response.statusText);
-//  			}
-//  			  dispatch(isLoading(false));
-//  			  return response;
-//  		}).then((response)=> response.json())
-//  		  .then(()=> dispatch(LogInSuccess(true)))
-//  		      .catch((err)=> dispatch(LogInFailed(false, err)));
-
-//  	};
-//  }
 
  /*export function ForgotPass(email){
  	return {type: types.FORGOTPass, recoverEmail: email};
