abfec02824c495011e13967693653e48eec32cbc
doyougnu <jmy6342@gmail.com>
2017-11-01 21:17:28 -0700

    Loading redux into the app

--- react-front-end/src/ReduxFolder/Store/configureStore.js
+++ react-front-end/src/ReduxFolder/Store/configureStore.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import tldrApp from "../Reducers/reducer.js";
+import tldrApp from "../Reducers/root";
 import { createLogger } from 'redux-logger';
 
 
 const loggerMiddleware = createLogger();
 // apply redux thunk here
-export default const store = createStore(
+export const store = createStore(
 	tldrApp,
 	applyMiddleware(
 		thunkMiddleware,
