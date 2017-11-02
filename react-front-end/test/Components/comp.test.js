8de668cdd7d1ac484508c37a9647403621d7fbe1
doyougnu <jmy6342@gmail.com>
2017-11-02 14:47:58 -0700

    Adding test to test compilation of side bar

--- react-front-end/test/Components/comp.test.js
+++ react-front-end/test/Components/comp.test.js
@@ -1,3 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SideNavCustom from '../../src/AppComponents/AppSideBar.js';
+
+describe('Side Navigator', () => {
+    it('renders the app side bar with crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<SideNavCustom/>, div);
+    });
+});
