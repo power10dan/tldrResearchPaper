6aadbb5ce71749167b16afe49225162da8203997
doyougnu <jmy6342@gmail.com>
2017-11-02 15:34:39 -0700

    Got a simple compile test working

--- react-front-end/src/tests/Components/comp.test.js
+++ react-front-end/src/tests/Components/comp.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SideNavCustom from '../../AppComponents/AppSideBar.js';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme'
+import configureStore from 'redux-mock-store'
+
+configure({ adapter: new Adapter() });
+
+const mockStore = configureStore();
 
 describe('Side Navigator', () => {
     it('renders the app side bar with crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<SideNavCustom/>, div);
+      const wrapper = shallow(<SideNavCustom/>, { context: { store: mockStore() } });
+      /* expect(wrapper.contains(<div title="User" />)).toBe(true);*/
     });
 });
