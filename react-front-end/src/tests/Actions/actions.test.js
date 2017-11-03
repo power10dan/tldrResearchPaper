98f6deb22cbca3075fd44d009681f9fc4b352d66
doyougnu <jmy6342@gmail.com>
2017-11-03 10:52:08 -0700

    More action tests, need to figure out login testing

--- react-front-end/src/tests/Actions/actions.test.js
+++ react-front-end/src/tests/Actions/actions.test.js
@@ -19,4 +19,36 @@ describe('actions', () => {
         };
         expect(actions.DeleteFile(text)).toEqual(expectedAction);
     });
+
+
+    it('should create an action to show loading stats', () => {
+        const text = 'stats';
+        const expectedAction = {
+            type: types.LOADING,
+            isLoading: text
+        };
+        expect(actions.isLoading(text)).toEqual(expectedAction);
+    });
+
+    it('should create an action to show login success', () => {
+        const text = true;
+        const expectedAction = {
+            type: types.LOGIN_SUCCESS,
+            isLogin: text
+        };
+        expect(actions.LogInSuccess(text)).toEqual(expectedAction);
+    });
+
+    it('should create an action to show Log In Failure', () => {
+        const text = false;
+        const message = "I failed at logging in";
+        const expectedAction = {
+            type: types.LOGIN_FAIL,
+            isLogin: text,
+            message: message
+        };
+        expect(actions.LogInFailed(text, message)).toEqual(expectedAction);
+    });
+
+    // TODO figure out async login testing
 });
