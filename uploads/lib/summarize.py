72da1c2df054e34d776b90fdbd0254dbc1d14909
LAPTOP-OPRF7ITC\Rahul <borkarra@gmail.com>
2017-11-03 14:44:40 -0700

    summarize script

---
+++ uploads/lib/summarize.py
@@ -0,0 +1,38 @@
+import xmltodict
+from collections import OrderedDict
+from sumy.parsers.plaintext import PlaintextParser #We're choosing a plaintext parser here, other parsers available for HTML etc.
+from sumy.nlp.tokenizers import Tokenizer 
+from sumy.summarizers.lex_rank import LexRankSummarizer #We're choosing Lexrank, other algorithms are also built in
+
+def summarize(document, headings_file):
+    headings = parse_headings(headings_file)
+    with open(document) as fd:
+        doc = xmltodict.parse(fd.read())
+        num_sections = len(doc['TEI']['text']['body']['div'])
+        sections = doc['TEI']['text']['body']['div']
+        for heading in range(num_sections):
+            current_heading = sections[heading]['head']
+            if(type(current_heading) == OrderedDict):
+                current_heading = current_heading['#text']
+            if current_heading in headings:
+                text = ""
+                for i in sections[heading]['p']:
+                    if(type(i) == OrderedDict):
+                        text += (i['#text'] + " ")
+                    else:
+                        text += (i + " ")
+                plain_text = text.encode('ascii','ignore')
+                parser = PlaintextParser.from_string(plain_text, Tokenizer("english"))
+                summarizer = LexRankSummarizer()
+                summary = summarizer(parser.document, 5)
+                sum_summary = ""
+                for sentence in summary:
+                    sum_summary += str(sentence) + " "
+                print("Summary for " + current_heading + ":\n\n" + sum_summary)
+
+def parse_headings(headings_file):
+    with open(headings_file) as headers:
+        for line in headers:
+            line_split = line.split(';;')
+            return line_split
+
