e72e3ad2828868ec7f1b7a0c8c0a886c36c612d0
doyougnu <jmy6342@gmail.com>
2017-11-03 10:26:06 -0700

    Guess we can't configure jest....great

--- react-front-end/src/tests/Components/AppSideBar.test.js
+++ react-front-end/src/tests/Components/AppSideBar.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SideNavCustom from '../../AppComponents/AppSideBar.js';
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme'
+import { shallow, configure } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
 import configureStore from 'redux-mock-store'
+
+configure({ adapter: new Adapter() });
 
 const mockStore = configureStore();
 
@@ -11,5 +14,6 @@ describe('Side Navigator', () => {
     it('renders the app side bar with crashing', () => {
       const wrapper = shallow(<SideNavCustom/>, { context: { store: mockStore() } });
       /* expect(wrapper.contains(<div title="User" />)).toBe(true);*/
+      expect(shallowToJson(wrapper)).toMatchSnapshot();
     });
 });
