113302149d5459f09c3c2d8beb8840e238bd054a
power10dan <lintzu@onid.oregonstate.edu>
2017-11-02 17:05:13 -0700

    Modified Naming Convention

--- react-front-end/src/ReduxFolder/Reducers/authentication.js
+++ react-front-end/src/ReduxFolder/Reducers/authentication.js
@@ -1,9 +1,9 @@
-import * as types from '../Constants/ActionTypes';
+import * as types from '../Constants/ActionTypes.js';
 
 let user = JSON.parse(localStorage.getItem('user'));
 const initialState = user ? { loggedIn: true, user } : {};
 
-export function authentication(state = initialState, action) {
+export default function authentication(state = initialState, action) {
     switch (action.type) {
     case types.LOGIN_REQUEST:
         return {
