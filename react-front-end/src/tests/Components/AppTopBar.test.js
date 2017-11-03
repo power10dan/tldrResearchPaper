0359c792bf17e81891be9d13487780d2cc395ebc
doyougnu <jmy6342@gmail.com>
2017-11-03 10:22:53 -0700

    Creating more tests, mount is failing for some reason

---
+++ react-front-end/src/tests/Components/AppTopBar.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppTopBar from '../../AppComponents/AppTopBar';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure} from 'enzyme';
+import configureStore from 'redux-mock-store';
+
+
+configure({ adapter: new Adapter() });
+
+const mockStore = configureStore();
+
+describe('Top Bar', () => {
+    it('renders the top bar with crashing', () => {
+        const wrapper = shallow(<AppTopBar/>, { context: { store: mockStore() } });
+    });
+});
