b184c12371de8f9945b9c4834d127ca200a6c37f
doyougnu <jmy6342@gmail.com>
2017-11-04 16:17:00 -0700

    Removing redundant login failure type

--- react-front-end/src/ReduxFolder/Constants/ActionTypes.js
+++ react-front-end/src/ReduxFolder/Constants/ActionTypes.js
@@ -6,13 +6,12 @@
  *
  */
 
-export const  LOGIN_SUCCESS   = 'LOGIN SUCCESS';
-export const  LOGIN_FAIL = 'LOGIN FAILED';
+export const  LOGIN_SUCCESS   = 'LOGIN_SUCCESS';
+export const  LOGIN_FAIL = 'LOGIN_FAILED';
 export const  LOADING = 'LOADING';
 export const  REQUEST = 'REQUEST';
 export const  LOGOUT = 'LOGOUT';
 export const  LOGIN_REQUEST = 'LOGIN_REQUEST';
-export const  LOGIN_FAILURE = 'LOGIN_FAILURE';
 export const  IS_DISABLE = 'IS_LOGIN_BUTTON_DISABLE';
 export const OPEN_DIALOG = 'OPEN_DIALOG';
 export const CLOSE_DIALOG = 'CLOSE_DIALOG';
