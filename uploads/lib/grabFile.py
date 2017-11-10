31496aaf0c6a0108d2f0a902f20a26ea9532dd40
doyougnu <jmy6342@gmail.com>
2017-11-09 16:29:48 -0800

    Removing unused library

--- uploads/lib/grabFile.py
+++ uploads/lib/grabFile.py
@@ -1,7 +1,6 @@
 from django.http import FileResponse
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.parsers import FileUploadParser, MultiPartParser
 
 import glob
 import base64
