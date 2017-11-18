aac0a6828b70d00da48e1d03bb148762498d7330
doyougnu <jmy6342@gmail.com>
2017-11-18 10:05:20 -0800

    naming convention fixed in create profile actions

--- react-front-end/src/ReduxFolder/Actions/CreateProfileActions.js
+++ react-front-end/src/ReduxFolder/Actions/CreateProfileActions.js
@@ -3,72 +3,81 @@ import { saveCred } from '../Actions/SaveCred.js';
 import { DialogOpenCreate , DialogCloseCreate } from './DialogActions.js';
 import { LogInSuccess, Login } from './LoginActions.js';
 
-function _createProfile(username, passwordFirst, passwordSecond, account_emailaddress) {
+function _createProfileAction(username,
+                              password_one,
+                              password_two,
+                              account_email_address) {
     let url = "http://127.0.0.1:8000/rest-auth/registration/";
     let request = {
 			  method: 'post',
 			  body: JSON.stringify({ username: username, 
-                               account_emailaddress: account_emailaddress, 
-                               password1: passwordFirst, 
-                               password2: passwordSecond
+                               account_emailaddress: account_email_address, 
+                               password1: password_one, 
+                               password2: password_two
                             }),
 			  headers: {
         	  'Content-Type': 'application/json'
         }
 		}; 
-    console.log("i am in create profile");
-    console.log(request)
     return fetch(url, request);
 }
 
-export function createProfile(userName, passWord, passWord2, userEmail){
+export function createProfileAction(user_name,
+                                    password_one,
+                                    password_two,
+                                    user_email) {
 	return dispatch => {
-  		  _createProfile(userName, passWord, passWord2, userEmail)
+  		  _createProfile(user_name, password_one, password_two, user_email)
+
             .then((response) => {
+
                 if(response.status === 201){
                     return response.json();
                 }
+
                 if(response.status === 400 ){
-                    dispatch(CreateFailed("Username already existied"));
+                    dispatch(createFailedAction("Username already exists"));
                     dispatch(DialogOpenCreate()),
                     setTimeout(()=>{dispatch(DialogCloseCreate())}, 2000);
                     return;
                 }
 
   		      }).then((data)=>{
+
                 if(typeof data !== 'undefined'){
-                    dispatch(CreateSuccess("Profile Created!"));
-                    dispatch(Login(userName, userEmail, passWord));
+
+                    dispatch(createSuccessAction("Profile Created!"));
+                    dispatch(Login(user_name, user_email, password_one));
                     dispatch(LogInSuccess("You have logged in!"));
                     dispatch(DialogOpenCreate()),
-                    setTimeout(()=>{dispatch(DialogCloseCreate())}, 2000);
-                    // save the token to the user profile
-                    // dispatch(saveCred(userName, userEmail, data.key));
+                    setTimeout(()=>{dispatch(DialogCloseCreate());}, 2000);
 
                 }
   		      }).catch((err) =>{
   			        if(err.message === "Failed to fetch"){
-                    dispatch(CreateFailed("Server Connection Refused. Are you connected to the WIFI?"));
+                    dispatch(createFailedAction(
+                        "Server Connection Refused. \
+                         Are you connected to the WIFI?"));
                 }
   		      });
 	   };
 }
 
-export function CreateFailed(errMessage){
+export function createFailedAction(errMessage){
     return {
         type: types.FAIL_CREATE,
         errorMess: errMessage
     };
 }
 
-export function CreateSuccess(successMess){
+export function createSuccessAction(successMess){
      return {
         type: types.CREATE_SUCCESS,
         successMessage: successMess 
      };
 }
 
-export function ResetDialog(){
+export function resetDialogAction(){
     return {
       type: types.RESET_DIALOG
     }
