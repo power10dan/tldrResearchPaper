3336417cba011d179857407c17510eb18066fb8c
doyougnu <jmy6342@gmail.com>
2017-11-21 16:22:25 -0800

    removing Grobid, forcing ui to be single columns

--- react-front-end/src/AppComponents/FileCard.js
+++ react-front-end/src/AppComponents/FileCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {withStyles} from 'material-ui/styles';
-import Card, {CardActions, CardContent} from 'material-ui/Card';
+import Card, {CardActions, CardContent, CardHeader} from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 const styles = theme => ({
@@ -10,7 +10,6 @@ const styles = theme => ({
 		borderRadius: "15px",
 		background: "#5C6BC0",
 		height: 185,
-		width: 395,
 	},
 
 	title: {
@@ -28,7 +27,7 @@ const styles = theme => ({
 		fontSize:14,
 		color: "#E8EAF6",
 		margingTop: "-10px"
-	}, 
+	},
 
 	cardAction:{
 		marginTop: "-65px",
@@ -53,22 +52,31 @@ const FileCardView =  (props) =>{
 	return (
 		<div>
 			<Card className={props.classes.card} >
+        <CardHeader className={props.classes.title}
+                    title={props.p_title}
+        />
 				<CardContent>
-					<Typography type="h2" className={props.classes.p_title}>
-			            {props.p_title}
-			        </Typography>
-				    <Typography type="h3" className={props.classes.p_summary_txt}>
-				        {"Intro Summary: " + props.p_summary_txt}
-				     </Typography>
+					{/* <Typography type="h2"
+              className={props.classes.p_title}>
+			        {props.p_title}
+			        </Typography> */}
+				  <Typography type="body2"
+                      className={props.classes.p_summary_txt}>
+				    {"Intro Summary: " + props.p_summary_txt}
+				  </Typography>
+			    <CardActions className={props.classes.cardAction}>
+			      <Button color="primary"
+                    className={props.classes.buttonStyle}
+                    onClick={props.p_card_dialog}>
+					    Add Summary
+					  </Button>
 
-			        <CardActions className={props.classes.cardAction}>
-			        	<Button color="primary" className={props.classes.buttonStyle} onClick={props.p_card_dialog}>
-					        Add Summary
-					     </Button>
-					      <Button color="primary" className={props.classes.buttonStyle} onClick={props.p_getPDF} >
-					        Download Paper
-					     </Button>
-			        </CardActions>
+					  <Button color="primary"
+                    className={props.classes.buttonStyle}
+                    onClick={props.p_getPDF} >
+					    Download Paper
+					  </Button>
+			    </CardActions>
 				</CardContent>
 			</Card>
 		</div>
