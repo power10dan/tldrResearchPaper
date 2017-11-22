1a9383cb012d139fa6e5c5e31bb34d6e6e1ea926
doyougnu <jmy6342@gmail.com>
2017-11-21 16:52:36 -0800

    Expandable cards completed

--- react-front-end/src/AppComponents/FileCard.js
+++ react-front-end/src/AppComponents/FileCard.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import {withStyles} from 'material-ui/styles';
-import Card, {CardActions, CardContent} from 'material-ui/Card';
+import classnames from 'classnames';
+import Card, {CardActions, CardContent, CardHeader} from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
+import Collapse from 'material-ui/transitions/Collapse';
 import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
+
 const styles = theme => ({
 	card: {
 		marginTop: "30px",
 		marginLeft: "35px",
 		borderRadius: "15px",
 		background: "#5C6BC0",
-		height: 185,
-		width: 395,
+		/* height: 185,*/
 	},
 
 	title: {
@@ -22,13 +26,28 @@ const styles = theme => ({
 		color: "#E8EAF6"
 	},
 
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+
+  flexGrow: {
+    flex: '1 1 auto',
+  },
+
 	summaryText:{
 		textAlign: "left",
 		marginBottom: "5px",
 		fontSize:14,
 		color: "#E8EAF6",
 		margingTop: "-10px"
-	}, 
+	},
 
 	cardAction:{
 		marginTop: "-65px",
@@ -49,30 +68,69 @@ const styles = theme => ({
   	}
 });
 
-const FileCardView =  (props) =>{
-	return (
-		<div>
-			<Card className={props.classes.card} >
-				<CardContent>
-					<Typography type="h2" className={props.classes.p_title}>
-			            {props.p_title}
-			        </Typography>
-				    <Typography type="h3" className={props.classes.p_summary_txt}>
-				        {"Intro Summary: " + props.p_summary_txt}
-				     </Typography>
 
-			        <CardActions className={props.classes.cardAction}>
-			        	<Button color="primary" className={props.classes.buttonStyle} onClick={props.p_card_dialog}>
-					        Add Summary
-					     </Button>
-					      <Button color="primary" className={props.classes.buttonStyle} onClick={props.p_getPDF} >
-					        Download Paper
-					     </Button>
-			        </CardActions>
-				</CardContent>
-			</Card>
-		</div>
-	);
+class FileCardView extends React.Component {
+  state = { expanded: false };
+
+  handleExpandClick = () => {
+    this.setState({ expanded: !this.state.expanded });
+  };
+
+
+  render () {
+    const { classes } = this.props;
+	  return (
+		  <div>
+			  <Card className={this.props.classes.card}>
+          <CardHeader className={this.props.classes.title}
+                      title={this.props.p_title}
+          />
+
+				  <CardContent>
+				    <Typography paragraph className={this.props.classes.p_summary_txt}>
+				      {"Intro Summary: " + this.props.p_summary_txt}
+				    </Typography>
+				  </CardContent>
+
+			    <CardActions className={this.props.classes.cardAction}>
+			      <Button color="primary"
+                    className={this.props.classes.buttonStyle}
+                    onClick={this.props.p_card_dialog}>
+					    Add Summary
+					  </Button>
+					  <Button color="primary"
+                    className={this.props.classes.buttonStyle}
+                    onClick={this.props.p_getPDF}>
+					    Download Paper
+					  </Button>
+            <div className={classes.flexGrow} />
+            <IconButton
+              className={classnames(classes.expand, {
+                  [classes.expandOpen]: this.state.expanded
+              })}
+              onClick={this.handleExpandClick}
+              aria-expanded={this.state.expanded}
+              aria-label="Show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+			    </CardActions>
+          <Collapse in={this.state.expanded} transitionDuration="auto" unmountOnExit>
+            <CardContent>
+              <Typography type="body2">
+                A HEADER!
+              </Typography>
+              <Typography paragraph>
+                Hi there, this is to demo that expanding actually works! This
+                text is hardcoded in the FileCard.js file in the frontend 
+                components. You should change this to be somethign that is
+                passed in from props in the future.
+              </Typography>
+            </CardContent>
+          </Collapse>
+			  </Card>
+		  </div>
+	  );}
 }
 
 export default withStyles(styles)(FileCardView);
