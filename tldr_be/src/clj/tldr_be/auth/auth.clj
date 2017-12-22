2d90a6ddf6b85fb99f69613456f3e838a203aa16
doyougnu <jmy6342@gmail.com>
2017-12-22 10:09:47 -0800

    adding auth-create-user and auth-user functions

--- tldr_be/src/clj/tldr_be/auth/auth.clj
+++ tldr_be/src/clj/tldr_be/auth/auth.clj
@@ -1,10 +1,10 @@
 (ns tldr-be.auth.auth
   (:require [cheshire.core :as json]
-            [tldr-be.db.core :refer [get-user-by-name-pass]]
-            [tldr-be.middleware :as middleware]
+            [tldr-be.db.core :refer [get-user-by-name create-user!]]
             [buddy.core.nonce :refer [random-bytes]]
             [buddy.auth.backends :as backends]
             [buddy.sign.jwt :as jwt]
+            [buddy.hashers :as hs]
             [clj-time.core :refer [plus now minutes]]
             [ring.util.response :refer [redirect]]))
 
@@ -23,15 +23,23 @@
                 :exp (plus (now) (minutes 60))}]
     (jwt/sign claims key)))
 
-(defn do-login
-  "Given a request, get the username and password out of the request, check that
+(defn auth-create-user!
+  "Given a first name, last name, email and password create a user with an
+  encrypted password"
+  [first_name last_name email pass]
+  (let [hashed_pass (hs/encrypt pass)]
+    (create-user! {:first_name first_name
+                   :last_name last_name
+                   :email email
+                   :pass hashed_pass})))
+
+(defn auth-user
+  "Given credentials, get the username and password out of the request, check that
   the user exists in the db, if good then return a 200 with a token, if not then
   redirect to the home page"
-  [request]
-  (let [data (:form-params request)]
-    (if-let [user (get-user-by-name-pass {:first_name (:username data)
-                                          :pass (:password data)})]
-      {:status 200
-       :headers {:content-type "application/json"}
-       :body (json/encode {:token (middleware/token user middleware/secret)})}
-      (redirect "/"))))
+  [creds]
+  (let [unauthed [false {:message "Invalid username or password"}]]
+    (if-let [user (get-user-by-name {:first_name (:username creds)})]
+      (when (hs/check (:password creds) (:pass user)) ;;verify passwords match
+        [true {:user (dissoc user :password)}])       ;;remove pass from token
+      unauthed)))
