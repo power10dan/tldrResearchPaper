b029a0fbddf7d33c647e99789d485ad5daa1295d
doyougnu <jmy6342@gmail.com>
2017-12-24 13:47:02 -0800

    using http-response lib

--- tldr_be/src/clj/tldr_be/routes/home.clj
+++ tldr_be/src/clj/tldr_be/routes/home.clj
@@ -1,10 +1,11 @@
 (ns tldr-be.routes.home
   (:require [tldr-be.layout :as layout]
             [compojure.core :refer [defroutes GET POST]]
-            [ring.util.http-response :as response]
+            [ring.util.http-response :as http]
             [buddy.auth.accessrules :refer [restrict]]
             [tldr-be.auth.handler :as handler]
             [tldr-be.auth.core :as auth]
+            [tldr-be.doc.core :as doc]
             [clojure.java.io :as io]))
 
 (defn home-page []
@@ -16,15 +17,14 @@
 
 (defn on-error
   [request value]
-  {:status 403
-   :headers {}
-   :body "Not Authorized"})
+  http/unauthorized "Not Authorized!")
 
 (defroutes home-routes
   (GET "/" [] (home-page))
   (GET "/about" [] (about-page)))
 
 (defroutes bus-routes ;;business-routes
-  (POST "/create-auth-token" [] handler/create-auth-token)
+  (POST "/api/uploadFile/" [] doc/insert-doc!)
+  (POST "/login/" [] handler/create-auth-token)
   (GET "/get-user" [] (restrict handler/get-user {:handler auth/is-auth?
                                                   :on-error on-error})))
