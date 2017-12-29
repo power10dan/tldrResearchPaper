b54ca86472327dc2043aab70f49cca5c3167d634
doyougnu <jmy6342@gmail.com>
2017-12-28 16:08:58 -0800

    use Higher order functions! downvote and upvote working

--- tldr_be/src/clj/tldr_be/summary/core.clj
+++ tldr_be/src/clj/tldr_be/summary/core.clj
@@ -1,7 +1,5 @@
 (ns tldr-be.summary.core
-  (:require [tldr-be.db.core :refer [create-summary!]]
-            [tldr-be.db.core :refer [up-votes!]]
-            [tldr-be.db.core :refer [down-votes!]]
+  (:require [tldr-be.db.core :refer [up-votes! down-votes!]]
             [ring.util.http-response :as http]))
 
 (defn insert-sum!
@@ -25,28 +23,10 @@
         [true "Summary successfully created"])
       [false "Request Malformed"])))
 
-(defn up-vote-sum!
-  "increment (by 1) the votes for a given summary"
-  [body]
-  (let [header (get body "header")
-        author (get body "author")
-        filename (get body "filename")
-        summary (get body "summary")
-        doc_id (get body "doc_id")]
-
-    (if (and header author filename summary doc_id)
-      (do ;; if we have all of the required information, up vote
-        (up-votes! {:header header
-                    :author author
-                    :filename filename
-                    :summary summary
-                    :doc_id doc_id})
-        [true "upvote succesful"])
-      [false "Request Malformed"])))
-
-(defn down-vote-sum!
-  "decrement (by 1) the votes for a given summary"
-  [body]
+(defn _vote-sum!
+  "Given a request body, a success message and a function that either up-votes or
+  down-votes, grab all necessary stuff out of the body and call the function"
+  [body msg f]
   (let [header (get body "header")
         author (get body "author")
         filename (get body "filename")
@@ -55,10 +35,18 @@
 
     (if (and header author filename summary doc_id)
       (do ;; if we have all of the required information, down vote
-        (down-votes! {:header header
-                      :author author
-                      :filename filename
-                      :summary summary
-                      :doc_id doc_id})
-        [true "downvote succesful"])
+        (f {:header header
+            :author author
+            :filename filename
+            :summary summary
+            :doc_id doc_id})
+        [true msg])
       [false "Request Malformed"])))
+
+(defn up-vote-sum!
+  [body]
+  (_vote-sum! body "upvote succesful" up-votes!))
+
+(defn down-vote-sum!
+  [body]
+  (_vote-sum! body "downvote successful" down-votes!))
