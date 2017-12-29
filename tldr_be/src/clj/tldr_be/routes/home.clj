c0d90c792eeab396255f8ee91c28726d1432108e
doyougnu <jmy6342@gmail.com>
2017-12-28 18:07:05 -0800

    better formatting

--- tldr_be/src/clj/tldr_be/routes/home.clj
+++ tldr_be/src/clj/tldr_be/routes/home.clj
@@ -26,9 +26,9 @@
   (GET "/about" [] (about-page)))
 
 (defroutes bus-routes ;;business-routes
-  (POST "/api/uploadFile/" [] doc/insert-doc!)
-  (POST "/api/addSummary/" [] sum_handler/insert-sum)
-  (POST "/api/sumUpVote/" [] sum_handler/up-vote-sum)
+  (POST "/api/uploadFile/"  [] doc/insert-doc!)
+  (POST "/api/addSummary/"  [] sum_handler/insert-sum)
+  (POST "/api/sumUpVote/"   [] sum_handler/up-vote-sum)
   (POST "/api/sumDownVote/" [] sum_handler/down-vote-sum)
   (POST "/login/" [] auth_handler/create-auth-token)
   (GET "/get-user" [] (restrict auth_handler/get-user {:handler auth/is-auth?
