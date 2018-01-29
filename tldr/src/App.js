6949f32c0474115f94976336f60c0a9a16533e5b
power10dan <lintzu@onid.oregonstate.edu>
2018-01-28 23:28:02 -0800

    FrontEndBranch Works With Local DataBase System, And Is Up-To-Date With Master

--- tldr/src/App.js
+++ tldr/src/App.js
@@ -6,18 +6,36 @@ import { withStyles } from 'material-ui/styles';
 import { styles, CustomizationList } from './AppComponent/CustomizationAction.js';
 import GetContentFromServer from './AppBusinessLogic/PaperDownloadLogic.js';
 import { DataSubscriptionDummyFunc } from './DummyData.js';
+import { ConferenceExpansionPanel } from './AppComponent/ExpansionPanelConferences.js';
 
 class App extends Component {
-  render() {
-  	let DownloadedContent = GetContentFromServer(CustomizationList, DataSubscriptionDummyFunc);
-	  let StyledCustomizationComponent = withStyles(styles)(DownloadedContent);
-    let CustomPage = DashBoardControlHOC(StyledCustomizationComponent);
-    return (
-      <div className="App">      
-          <CustomPage />
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state={
+            CurrPage: 0,
+        }
+    }
+
+    componentWillReceiveProps(nextProp){
+        this.setState({CurrPage: nextProp.currPage});
+    }
+
+    render() {
+    	let DownloadedContent = GetContentFromServer(CustomizationList, DataSubscriptionDummyFunc);
+  	  let StyledCustomizationComponent = withStyles(styles)(DownloadedContent);
+      let CustomPage = null;
+      if(this.state.CurrPage === 0){
+          CustomPage = DashBoardControlHOC(StyledCustomizationComponent);
+      } else {
+          CustomPage = DashBoardControlHOC(ConferenceExpansionPanel);
+      }
+
+      return (
+        <div className="App">      
+            <CustomPage />
+        </div>
+      );
+    }
 }
 
 export default App;
--- tldr_be/profiles.clj
+++ tldr_be/profiles.clj
@@ -2,6 +2,6 @@
 ;; The profiles.clj file is used for local environment variables, such as database credentials.
 ;; This file is listed in .gitignore and will be excluded from version control by Git.
 
-{:profiles/dev  {:env {:database-url "jdbc:postgresql://localhost/tldr_be_dev?user=db_user_name_here&password=db_user_password_here"
+{:profiles/dev  {:env {:database-url "jdbc:postgresql://localhost/tldr_database?user=daniellin&password=soccer2005^&"
                        :neo4j-db-url "http://neo4j:password@localhost:7474/db/data"}}
- :profiles/test {:env {:database-url "jdbc:postgresql://localhost/tldr_be_test?user=db_user_name_here&password=db_user_password_here"}}}
+ :profiles/test {:env {:database-url "jdbc:postgresql://localhost/tldr_database?user=daniellin&password=soccer2005^&"}}}
