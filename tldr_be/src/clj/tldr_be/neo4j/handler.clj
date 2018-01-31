c255729158cacccc1f0c2a4f5ddb01ad975cbd0b
doyougnu <jmy6342@gmail.com>
2018-01-30 17:20:46 -0800

    query param name fix

--- tldr_be/src/clj/tldr_be/neo4j/handler.clj
+++ tldr_be/src/clj/tldr_be/neo4j/handler.clj
@@ -10,7 +10,7 @@
   of valid titles and ids"
   [req]
   (let [_ts (get-in req [:query-params "title"])
-        _is (get-in req [:query-params "ids"])
+        _is (get-in req [:query-params "id"])
         ts (cond (coll? _ts) _ts (not-nil? _ts) (vector _ts) :else nil)
         is (cond (coll? _is) _is (not-nil? _is) (vector _is) :else nil)]
     (distinct (concat
@@ -25,6 +25,17 @@
   paper with no duplicates"
   [req]
   (let [[ok? res] (apply neo/get-all-children (massage-req req))]
+    (if ok?
+      {:status 200
+       :headers {"Content-Type" "application/json"}
+       :body res}
+      (http/bad-request res))))
+
+(defn get-all-shared-children
+  "Given a request that specifies a n-many paper titles get all children for each
+  paper with no duplicates"
+  [req]
+  (let [[ok? res] (apply neo/get-all-shared-children (massage-req req))]
     (if ok?
       {:status 200
        :headers {"Content-Type" "application/json"}
