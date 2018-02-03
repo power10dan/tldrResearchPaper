964a3592675ef6f0c89b67bb32c20c915d659593
doyougnu <jmy6342@gmail.com>
2018-02-03 14:04:43 -0800

    adding db configs

--- tldr_be/profiles.clj
+++ tldr_be/profiles.clj
@@ -5,5 +5,5 @@
 {:profiles/dev  {:env {:database-url "jdbc:postgresql://localhost/tldr_be_dev?user=db_user_name_here&password=db_user_password_here"
                        :neo4j-db-url "http://neo4j:password@localhost:7474/db/data"}}
  :profiles/test {:env {:database-url "jdbc:postgresql://localhost/tldr_be_test?user=db_user_name_here&password=db_user_password_here"}}
- :profiles/prod {:env {:database-url "jdbc:postgresql://maqcdhaohcqvdj:1e3eaf3ec6f80960c04c22c508715e77992647caa978fdb8b2a4f1dfbea3f373@ec2-54-197-233-123.compute-1.amazonaws.com:5432/d2gc3opuk7ubp6d2gc3opuk7ubp6?user=maqcdhaohcqvdj&password=1e3eaf3ec6f80960c04c22c508715e77992647caa978fdb8b2a4f1dfbea3f373"
-                       :neo4j-db-url "https://app87227172-fByoXx:b.oZ3vOCbuU7Rp.Bbc7YK5UUx8tdP1J@app87227172-fByoXx:b.oZ3vOCbuU7Rp.Bbc7YK5UUx8tdP1J@hobby-gbfekckcemaigbkejaleknal.dbs.graphenedb.com:24780"}}}
+ :profiles/prod {:env {:database-url "jdbc:postgresql://zjueouvmmbjbvz:20a795e87069343d5b283c3974ea061bc420036c88a27decd07752649c2344ba@ec2-54-227-244-122.compute-1.amazonaws.com:5432/d2b59nievcgn3p?user=zjueouvmmbjbvz&password=20a795e87069343d5b283c3974ea061bc420036c88a27decd07752649c2344ba"
+                       :neo4j-db-url "https://app87302872-N2QaDg:b.4rsLGYPKQfiz.w7QaJuQk1c4ocmxv@hobby-cjlamlpgpijggbkenjcimnal.dbs.graphenedb.com:24780"}}}
