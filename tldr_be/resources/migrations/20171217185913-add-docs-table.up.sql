b781a7d651c20ecb6255fe8dbf035177ef52456a
doyougnu <jmy6342@gmail.com>
2018-02-13 16:01:10 -0800

    adding titles to postgres tables

--- tldr_be/resources/migrations/20171217185913-add-docs-table.up.sql
+++ tldr_be/resources/migrations/20171217185913-add-docs-table.up.sql
@@ -1,4 +1,5 @@
 CREATE TABLE docs
 (id SERIAL PRIMARY KEY,
  filename VARCHAR(300),
+ title VARCHAR(300),
  filestuff BYTEA);
--- tldr_be/resources/migrations/20180123194922-xml-refs.up.sql
+++ tldr_be/resources/migrations/20180123194922-xml-refs.up.sql
@@ -1,4 +1,5 @@
 CREATE TABLE xml_refs
 (id INT PRIMARY KEY,
 filename VARCHAR(300),
+title VARCHAR(300),
 xml_content VARCHAR);
