8b5484da3a180fe82984133d2005c120214d0b2c
doyougnu <jmy6342@gmail.com>
2018-02-14 20:59:07 -0800

    insert is now memoized for titles!

--- tldr_be/src/clj/tldr_be/doc/core.clj
+++ tldr_be/src/clj/tldr_be/doc/core.clj
@@ -7,16 +7,19 @@
                                      create-xml-refs!
                                      create-xml-headers!
                                      get-xml-refs-by-name
-                                     get-xml-headers-by-name
+                                     get-xml-headers-by-title
+                                     get-headers-id-by-title
                                      *neo4j_db*]]
-            [clojure.string :refer [split]]
             [byte-streams :as bs]
             [clojure.xml :as xml]
             [clojure.tools.logging :as log]
             [tldr-be.utils.core :refer [parse-int]]
             [tldr-be.doc.pdf-parse :as pdf]
             [tldr-be.doc.engines :as eng]
-            [tldr-be.utils.core :refer [collect]]
+            [tldr-be.utils.core :refer [collect
+                                        get-basename
+                                        make-sections
+                                        get-sections]]
             [clojure.java.io :as io]))
 
 
@@ -44,8 +47,8 @@
 (defn insert-xml-headers!
   "Given params from a request, pull the filename and a xml of file data, insert
   that xml into the xml headers table in the db"
-  [fname xml_content]
-  (eng/insert-xml-engine fname xml_content create-xml-headers!))
+  [fname xml_content title]
+  (eng/insert-xml-engine fname xml_content title create-xml-headers!))
 
 
 (defn get-xml-refs-by-filename
@@ -55,11 +58,11 @@
   (eng/get-xml-engine name get-xml-refs-by-name))
 
 
-(defn get-xml-headers-by-filename
+(defn get-headers-by-title
   "Given the name of a file, if the filename is good
   then query the db for the corresponding xml headers by the filename"
-  [name]
-  (eng/get-xml-engine name get-xml-headers-by-name))
+  [title]
+  (eng/get-xml-engine title get-xml-headers-by-title))
 
 
 (defn pdf-to-xml-refs
@@ -77,7 +80,7 @@
   the file and return the parsed xml headers from the file"
   [filemap]
   (eng/pdf-to-xml-engine filemap
-                         get-xml-headers-by-filename
+                         get-headers-by-title
                          insert-xml-headers!
                          pdf/pdf-header-parser))
 
@@ -109,34 +112,13 @@
   (-> fblob f xml-to-map))
 
 
-(defn get-sections
- "take a nested xml clojure representation and pull out
-  tags and respective content when content is a string"
-  [grobid-map]
-  (for [x (xml-seq grobid-map)
-        :when (string? (first (:content x)))]
-    [(if (= (:tag x) :biblScope)
-       (-> x :attrs :unit keyword)
-       (:tag x))
-     (first (:content x))]))
-
-
-;; TODO fix all this post processing
-(defn make-sections
-  "Given a vector of vectors, like the output of get-sections, group by title
-  and add the title keyword to each entry"
-  [vec_o_vecs]
-  (->> vec_o_vecs
-       flatten
-       (partition-by #(= :title %))
-       (filter #(not (= :title (first %))))
-       (map #(doall (cons :title %)))))
-
-
 (defn process-headers
   [fmap]
   (->> fmap
        pdf-to-xml-headers
+       .getBytes
+       io/input-stream
+       xml/parse
        get-sections
        make-sections
        (map collect)
@@ -149,17 +131,16 @@
   [params]
   (let [filename (get-in params [:file :filename])
         file_blob (get-in params [:file :tempfile])]
-    (println (process-headers {:filename filename
-                               :filestuff file_blob}))
     (if (and filename file_blob)
       ;; if we have both filename and blob then perform the effect
       ;; we have to hit grobid everytime to get the title because filenames aren't trustworthy
-      (when-let [title (:title (process-headers {:filename filename
-                                                 :filestuff file_blob}))]
-        (do
-          (create-doc! {:filename (first (split filename #"\.")) ;;fname is basename
+      (when-let [heds (process-headers {:filename filename
+                                            :filestuff file_blob})]
+        (let [title (-> heds :title first)]
+          (create-doc! {:filename (get-basename filename) ;;fname is basename
                         :filestuff (bs/to-byte-array file_blob)
-                        :title (process-headers title)})
+                        :title title
+                        :pgid (:pgid (get-headers-id-by-title {:title title}))})
           [true "Your document successfully uploaded"]))
       [false "Request Malformed"])))
 
@@ -172,6 +153,7 @@
        make-sections
        (map collect)
        (filter #(contains? % :surname))))
+
 
 (defn workhorse
   "given a filename, grab the file bytea blob out of the db, parse the headers and
--- tldr_be/src/clj/tldr_be/doc/engines.clj
+++ tldr_be/src/clj/tldr_be/doc/engines.clj
@@ -1,23 +1,28 @@
-(ns tldr-be.doc.engines)
+(ns tldr-be.doc.engines
+  (:require [clojure.java.io :as io]
+            [clojure.xml :as xml]
+            [tldr-be.db.core :refer [get-headers-id-by-title]]
+            [tldr-be.utils.core :refer [collect get-sections make-sections]]))
 
 
 (defn insert-xml-engine
   "Given required fields id, filename, and xml content, and a function. Use the
   supplied function to insert the xml in the database that the function
   specifies"
-  [filename xml_content f]
-  (when (and filename xml_content)
-    (do ;; if we have all parameters, create an entry
+  [filename xml_content title f]
+  (when (and filename xml_content title)
+    (if (empty? (get-headers-id-by-title {:title title})) ;; if doc doesnt exist
       (f {:filename filename
-          :xml_content xml_content})
+          :xml_content xml_content
+          :title title})
       "Your document successfully uploaded")))
 
 
 (defn get-xml-engine
   "given the name of a file and a function that dictates a database. Use the
   supplied function to pull out the xml from the appropriate database table"
-  [name f]
-  (when name (f {:filename name})))
+  [title f]
+  (when title (f {:title title})))
 
 
 (defn pdf-to-xml-engine
@@ -28,9 +33,18 @@
   process it with pdf_parser_f"
   [filemap db_get db_put pdf_parser_f]
   (let [{id :id fname :filename fileblob :filestuff title :title} filemap
-        cached_xml (db_get fname)]
+        cached_xml (db_get title)]
     (if cached_xml
       (:xml_content cached_xml)
-      (let [{xml_file :body} (pdf_parser_f fileblob)]
-        (db_put fname xml_file)
+      (let [{xml_file :body} (pdf_parser_f fileblob)
+            title (->> xml_file
+                       .getBytes
+                       io/input-stream
+                       xml/parse get-sections
+                       make-sections
+                       (map collect)
+                       second
+                       :title
+                       first)]
+        (db_put fname xml_file title)
         xml_file))))
