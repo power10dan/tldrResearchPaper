3fa57d14aacae58cf1edab5b16582369465ac763
doyougnu <jmy6342@gmail.com>
2018-02-16 15:08:21 -0800

    basic search is working

---
+++ tldr_be/src/clj/tldr_be/search/core.clj
@@ -0,0 +1,41 @@
+(ns tldr-be.search.core
+(:require [clj-http.client :as http]
+          [clj-http.util :refer [url-encode]]
+          [clojure.data.xml :as xml]
+          [cheshire.core :as csh]
+          [tldr-be.utils.core :refer [not-nil?]]
+          [clojure.data.json :as json]))
+
+
+(defn parse-opts
+  "convert options to a uri query string fragment"
+  [opts]
+  (clojure.string/join (map (fn [[k v]]
+         (let [k (name k)
+               v (if (keyword? v) (name v) v)]
+           (str "&" k "=" v))) opts)))
+
+
+(defn encode-quotes
+  "wraps argument in encoded single quotes and uri encodes it"
+  [{term :term encode :encode}]
+  (let [term (if (= encode false) term (url-encode term))]
+    (str "%27" term "%27")))
+
+
+;; use like (search :web "propositions as types")
+(defn search
+  "https request to bing api, returns {:result <parsed body> :response <http
+  response map>} Example use: (search :web \"propositions as types\")"
+  ([bing-key inf term] (search inf term {}))
+  ([bing-key inf term opts]
+   (if bing-key
+     (let [params (parse-opts (dissoc opts :encode))
+           term (encode-quotes {:term term :encode (opts :encode)})
+           inf (clojure.string/capitalize (name inf))
+           headers {:headers {:Ocp-Apim-Subscription-Key bing-key}}
+           url (str "https://api.cognitive.microsoft.com/bing/v7.0/search"
+                    "?q=" term params)
+           resp (http/get url headers)]
+       (csh/parse-string (:body resp) keyword))
+     nil)))
