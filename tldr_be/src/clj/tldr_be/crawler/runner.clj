fb221eeae4db6f3f4f2b350b9208d3ff1bb08f0b
doyougnu <jmy6342@gmail.com>
2018-02-19 15:54:45 -0800

    adding bing-limit instead of hard code

--- tldr_be/src/clj/tldr_be/crawler/runner.clj
+++ tldr_be/src/clj/tldr_be/crawler/runner.clj
@@ -13,11 +13,11 @@
 (defn populate
   "Given nothing, grab the most sparse children in the graph and try to find
   their pdfs, then add them to the neo4j database"
-  [] (-> (neo/get-sparse-nodes 10) (doseq-interval add-paper 3000)))
+  [] (-> (neo/get-sparse-nodes (:bing-limit env))
+         (doseq-interval add-paper 3000)))
 
 
 (defn run-schedule
   "run some function that takes no args at the defined schedule"
   [f]
-  (let [t (time/today-at 3 00)]
-    (schedule f (-> (at t) :every :day))))
+  (let [t (time/today-at 3 00)] (schedule f (-> (at t) :every :day))))
